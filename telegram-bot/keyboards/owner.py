57a3001b0e92271c6bec333793ac7d20d8351b4b
maulananabil526-png <maulananabil526@gmail.com>
2026-01-16 19:10:26 +0700

    Create owner.py

---
+++ telegram-bot/keyboards/owner.py
@@ -0,0 +1,14 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from config import OWNER_USERNAME   # kita pakai config
+
+def kb_owner():
+    return InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(
+                "👑 Owner",
+                url=f"https://t.me/{OWNER_USERNAME}"
+            )
+        ]
+    ])
+
+
